f7a88b0faab0fd146c33a3cd7f68a27924874be2
kazirobinking <kazirobinking@gmail.com>
2026-03-14 21:26:23 +0600

    organization code

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router";
 import Footer from "@/components/footer/Footer";
 import NavbarWithNavigation from "@/components/navbar-with-navigation";
-import { ThemeProvider } from "@/components/theme/theme-provider";
 import DesktopNavigateContent from "@/components/navbar-with-navigation/DesktopNavigateContent";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const RootLayout = () => {
   return (
